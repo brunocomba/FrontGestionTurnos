427400a0859ca43a3b9a672a43cbaa592722c918
Bruno Comba <brcomba9@gmail.com>
2024-09-25 16:50:20 -0300

    ss

--- MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Debug/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7a8bf8ead35d299b76c52639fc65a017724b72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d8e51bea4c598613bb85d517c19054d31d5169")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MVC/obj/Release/net8.0/MVC.AssemblyInfo.cs
+++ MVC/obj/Release/net8.0/MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db7a8bf8ead35d299b76c52639fc65a017724b72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f87c68a4dc2288c41e63403769af200d38ed657")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
